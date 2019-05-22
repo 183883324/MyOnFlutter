86df73137898cc7907e021d64e21085e2bc8832d
liujun883324 <183883324@qq.com>
2019-05-22 17:24:47 +0800

    。。

--- lib/demo/BottomNavigationBarDemo.dart
+++ lib/demo/BottomNavigationBarDemo.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 
 class BoottomNacigationBarDemo extends StatefulWidget {
-
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _BoottomNacigationBarDemoState();
   }
-
 }
 
 class _BoottomNacigationBarDemoState extends State<BoottomNacigationBarDemo> {
@@ -15,7 +13,7 @@ class _BoottomNacigationBarDemoState extends State<BoottomNacigationBarDemo> {
 
   void _onTapHandler(int index) {
     setState(() {
-        _currentIndex=index;
+      _currentIndex = index;
     });
   }
 
@@ -30,17 +28,13 @@ class _BoottomNacigationBarDemoState extends State<BoottomNacigationBarDemo> {
       currentIndex: _currentIndex,
 //默认选择第一个
       items: [
-        BottomNavigationBarItem(
-            icon: Icon(Icons.explore), title: Text("导航")),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.history), title: Text("搜索")),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.list), title: Text("列表")),
-        BottomNavigationBarItem(
-            icon: Icon(Icons.person), title: Text("我的")),
+        BottomNavigationBarItem(icon: Icon(Icons.explore), title: Text("导航")),
+        BottomNavigationBarItem(icon: Icon(Icons.history), title: Text("搜索")),
+        BottomNavigationBarItem(icon: Icon(Icons.list), title: Text("列表")),
+        BottomNavigationBarItem(icon: Icon(Icons.person), title: Text("我的")),
       ],
       onTap: _onTapHandler,
+      backgroundColor: Colors.black12,
     );
   }
 }
-
